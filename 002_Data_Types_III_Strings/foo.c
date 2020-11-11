36684dde4171d9fc5604daa24e8a3447d000b9ba
Zygimantus <zygimantus@gmail.com>
2020-11-11 09:05:33 +0200

    Strings example

---
+++ 002_Data_Types_III_Strings/foo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	char message[12] = "Hello World";
+
+	printf("message is %s\n", message);
+
+	printf("array 2 is %c\n", message[1]);
+
+	return 0;
+}
