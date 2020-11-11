bec47262114763db773e414ee0befb6a7ce4adbd
Zygimantus <zygimantus@gmail.com>
2020-11-11 12:46:49 +0200

    Open read write example

---
+++ 024_Open_read_write/foo.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int fd;
+	char buf[14];
+
+	/* write */
+	fd = open("myfile.txt", O_CREAT | O_WRONLY, 0600);
+
+	if(fd == -1)
+	{
+		printf("Failed to create and open the file.\n");
+		exit(1);
+	}
+
+	write(fd, "Hello World!\n", 13);
+
+	close(fd);
+
+	/* read */
+	fd = open("myfile.txt", O_RDONLY);
+
+	if(fd == -1)
+	{
+		printf("Failed to open and read the file.\n");
+		exit(1);
+	}
+
+	read(fd, buf, 13);
+	buf[13] = '\0';
+
+	close(fd);
+
+	printf("buf: %s\n", buf);	
+
+	return 0;
+}
