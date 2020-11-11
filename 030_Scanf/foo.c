def8a0610e2571a0c038ed5fc86f466b61d076f2
Zygimantus <zygimantus@gmail.com>
2020-11-11 18:31:16 +0200

    Scanf example

---
+++ 030_Scanf/foo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int input;
+	char name[10];
+
+	//scanf("%d", &input);
+	scanf("%s", name);
+
+	//printf("input = %d\n", input);
+	printf("name = %s\n", name);
+
+	return 0;
+}
