de6a41d77d668fe465154419f077dbe3695be4d2
Zygimantus <zygimantus@gmail.com>
2020-11-11 09:14:03 +0200

    Pointers example

---
+++ 002_Data_Types_IV_Pointers/foo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int n;
+	int *k;
+
+	n = 25;
+	k = &n;
+//	*k = 25;
+
+	printf("n = %d\n", n);
+	printf("k = %x\n", k);
+	printf("*k = %d\n", *k);
+
+	return 0;
+}
