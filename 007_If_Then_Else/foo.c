828f959e1d678a2872e287b853dc562fa5dbe2b9
Zygimantus <zygimantus@gmail.com>
2020-11-11 09:28:14 +0200

    If then else example

---
+++ 007_If_Then_Else/foo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int n = 10;
+
+	if(n >= 5)
+	{
+		printf("n is greater or equal than to 5\n");
+	}
+	else
+	{
+		printf("Conditional statement 1 is false\n");
+	}
+
+	if(n <= 5)
+	{
+		printf("n is less than or equal to 5\n");
+	}
+	else
+	{
+		printf("Conditional statement 2 is false\n");
+	}
+
+	return 0;
+}
