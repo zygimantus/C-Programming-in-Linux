074b49ae512418259382ec3a462575cdb7dc0510
Zygimantus <zygimantus@gmail.com>
2020-11-11 10:05:46 +0200

    Function pointers example

---
+++ 013_Function_Pointers/foo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int add_numbers(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return sum;
+}
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	int (*myfunc_ptr)(int, int);
+
+	myfunc_ptr = &add_numbers;
+
+	result = myfunc_ptr(3, 7);
+
+	printf("result is %d\n", result);
+
+	return 0;
+}
