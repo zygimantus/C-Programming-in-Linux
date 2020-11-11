2143f8bc0a7ce5b3ed53895547447915cd51acec
Zygimantus <zygimantus@gmail.com>
2020-11-11 12:34:20 +0200

    Header file example

---
+++ 023_Header_file/foo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "myheader.h"
+
+int main(int argc, char *argv[])
+{
+	printf("sum = %d\n", addnum(7,4));
+	printf("sum2 = %d\n", addnumbers(6,5));
+
+	return 0;
+}
---
+++ 023_Header_file/myheader.h
@@ -0,0 +1,7 @@
+#ifndef _MYHEADER__H__
+#define _MYHEADER__H__
+
+#define addnum(x,y) x+y
+int addnumbers(int a, int b);
+
+#endif
